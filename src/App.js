e7fd8ed9778b5f34084b8f9ad2c5a8d11a9c602e
DuongThienPhuoc <phuocvip2@gmail.com>
2023-06-09 15:29:37 +0700

    edit App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Group 4 - Movie Project Main
-          _jad_asdaw_asdsad
-        </a>
-      </header>
-    </div>
+    <h1>Group 4 - Project</h1>
   );
 }
 
